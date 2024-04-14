9ce10c89de457894164c07077f620e6b5b7b1d2f
Vitaly Garshin <garshin.dev@gmail.com>
2024-04-14 20:26:28 +0300

    setup vite-svg-loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
+import vue from '@vitejs/plugin-vue'
+import svgLoader from 'vite-svg-loader'
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    svgLoader()
   ],
   resolve: {
     alias: {
