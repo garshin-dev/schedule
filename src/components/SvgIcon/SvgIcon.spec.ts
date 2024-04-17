5bbac6de308fa150543ee811675cb45f8c4558b5
Vitaly Garshin <garshin.dev@gmail.com>
2024-04-17 23:35:00 +0300

    add second test for SvgIcon and fix name of the first

--- src/components/SvgIcon/SvgIcon.spec.ts
+++ src/components/SvgIcon/SvgIcon.spec.ts
@@ -3,7 +3,17 @@ import { shallowMount } from '@vue/test-utils'
 import SvgIcon from '@/components/SvgIcon'
 
 describe('SvgIcon', () => {
-  it('should render', () => {
+  it('renders when name prop is provided', () => {
+    const wrapper = shallowMount(SvgIcon, {
+      props: {
+        name: 'notification',
+      },
+    })
+
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('does not render when name prop is not provided', () => {
     const wrapper = shallowMount(SvgIcon, {
       props: {
         name: 'notification',
