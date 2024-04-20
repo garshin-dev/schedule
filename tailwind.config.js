9dc032934cb55a4fff481a849c80d6f157db781f
Vitaly Garshin <garshin.dev@gmail.com>
2024-04-20 11:16:59 +0300

    add colors for tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ export default {
     hoverOnlyWhenSupported: true
   },
   theme: {
+    colors: {
+      'white': '#fff',
+      'black': '#333D54',
+      'red': '#FE4B4B',
+      'green': '#28F995',
+      'blue': '#3965FF',
+      'gray-primary': '#A8B3CC',
+      'gray-secondary': '#D9D9D9',
+    },
     extend: {},
   },
   plugins: [],
