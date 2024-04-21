2b26682f9a124c00670120f42937459d2635abe9
Vitaly Garshin <garshin.dev@gmail.com>
2024-04-21 14:24:52 +0300

    add compares test for AppCheckbox

--- src/components/AppCheckbox/AppCheckbox.spec.ts
+++ src/components/AppCheckbox/AppCheckbox.spec.ts
@@ -5,21 +5,41 @@ import AppCheckbox from '@/components/AppCheckbox'
 describe('AppCheckbox', () => {
   it('renders component correctly', () => {
     const wrapper = shallowMount(AppCheckbox)
+    const label = wrapper.find('label')
+    const input = wrapper.find('input')
+
     expect(wrapper.exists()).toBeTruthy()
+    expect(label.exists()).toBeTruthy()
+    expect(input.exists()).toBeTruthy()
   })
 
   it('compares label for and input id', () => {
     const wrapper = shallowMount(AppCheckbox)
-
     const label = wrapper.find('label')
     const input = wrapper.find('input')
 
-    expect(label.exists()).toBeTruthy()
-    expect(input.exists()).toBeTruthy()
     expect(label.attributes().for === input.attributes().id).toBeTruthy()
+  })
+
+  it('does not contain label text when label prop is not provided', () => {
+    const wrapper = shallowMount(AppCheckbox)
+    const label = wrapper.find('label')
+
+    expect(label.text().length).toBeFalsy()
+  })
+
+  it('contains label text when label prop is provided', () => {
+    const labelText = 'some label text'
+    const wrapper = shallowMount(AppCheckbox, {
+      props: {
+        label: labelText,
+      },
+    })
+
+    const label = wrapper.find('label')
+    expect(label.text() === labelText).toBeTruthy()
   })
 })
 
-// label
 // disabled
 // name
