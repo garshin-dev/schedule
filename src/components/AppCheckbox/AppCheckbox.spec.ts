ecafe17ebe9a9eda247ddd9c050f5e0c27281fbd
Vitaly Garshin <garshin.dev@gmail.com>
2024-04-21 14:11:55 +0300

    add new test for AppCheckbox

--- src/components/AppCheckbox/AppCheckbox.spec.ts
+++ src/components/AppCheckbox/AppCheckbox.spec.ts
@@ -5,7 +5,21 @@ import AppCheckbox from '@/components/AppCheckbox'
 describe('AppCheckbox', () => {
   it('renders component correctly', () => {
     const wrapper = shallowMount(AppCheckbox)
-
     expect(wrapper.exists()).toBeTruthy()
   })
+
+  it('compares label for and input id', () => {
+    const wrapper = shallowMount(AppCheckbox)
+
+    const label = wrapper.find('label')
+    const input = wrapper.find('input')
+
+    expect(label.exists()).toBeTruthy()
+    expect(input.exists()).toBeTruthy()
+    expect(label.attributes().for === input.attributes().id).toBeTruthy()
+  })
 })
+
+// label
+// disabled
+// name
