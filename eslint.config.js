dc801d8b68c0e9bb77217729a85a840c0e0ab3cf
Vitaly Garshin <garshin.dev@gmail.com>
2024-12-24 22:04:03 +0300

    add new config

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintPluginTailwindcss from 'eslint-plugin-tailwindcss'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    }
+  },
+  {
+    plugins: {
+      tailwindcss: eslintPluginTailwindcss,
+    },
+  },
+  ...vueTsEslintConfig(),
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/*.test.ts'],
+  },
+  skipFormatting,
+  eslintConfigPrettier
+]
