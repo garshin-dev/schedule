61fac25a573646b491f5ae961287a9d74efae09d
Vitaly Garshin <garshin.dev@gmail.com>
2024-12-25 21:29:41 +0300

    fix header

--- src/widgets/header/ui/Header.vue
+++ src/widgets/header/ui/Header.vue
@@ -1,9 +1,26 @@
 <template>
-  <header>
-    Header
+  <header class="border-b border-b-black/30 px-3 py-4">
+    <ul class="flex gap-4">
+      <li v-for="link in links" :key="link.name">
+        <RouterLink v-slot="{ isActive }" :to="link.path">
+          <span :class="[ isActive ? 'text-black' : 'text-black/50']">
+            {{ link.name }}
+          </span>
+        </RouterLink>
+      </li>
+    </ul>
   </header>
 </template>
 
 <script setup lang="ts">
-
+const links = [
+  {
+    name: 'Main',
+    path: '/'
+  },
+  {
+    name: 'Schedule',
+    path: '/schedule'
+  }
+]
 </script>
