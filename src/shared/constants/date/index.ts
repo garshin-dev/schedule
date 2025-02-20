f7f124b400da5670cdc75a2eb3476aab6055a06f
Vitaly Garshin <garshin.dev@gmail.com>
2025-02-20 23:33:57 +0300

    add constants, lib

---
+++ src/shared/constants/date/index.ts
@@ -0,0 +1,24 @@
+export const WEEKS: string[] = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday",
+];
+
+export const MONTHS: string[] = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december"
+];
