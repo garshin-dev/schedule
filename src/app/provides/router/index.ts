c477b06d5cb96957649a501eb79b22f1f81b4a47
Vitaly Garshin <garshin.dev@gmail.com>
2025-02-25 22:05:08 +0300

    fix router

--- src/app/provides/router/index.ts
+++ src/app/provides/router/index.ts
@@ -9,9 +9,31 @@ const router = createRouter({
       component: () => import('@/pages/home/ui/Home.vue'),
     },
     {
-      path: '/schedule/:year?/:month?/:week?/:startDay?/:endDay?',
+      path: '/schedule',
       name: 'schedule',
       component: () => import('@/pages/schedule/ui/Schedule.vue'),
+      children: [
+        {
+          path: 'year/:year',
+          name: 'schedule-year',
+          component: () => import('@/pages/schedule/ui/Schedule.vue'),
+        },
+        {
+          path: 'month/:year/:month',
+          name: 'schedule-month',
+          component: () => import('@/pages/schedule/ui/Schedule.vue'),
+        },
+        {
+          path: 'week/:year/:week',
+          name: 'schedule-week',
+          component: () => import('@/pages/schedule/ui/Schedule.vue'),
+        },
+        {
+          path: 'days/:year/:week/:startDay/:endDay?',
+          name: 'schedule-days',
+          component: () => import('@/pages/schedule/ui/Schedule.vue'),
+        },
+      ],
     },
   ],
 })
