1441c20694eca77604241d7354d5e1a927b61547
Vitaly Garshin <garshin.dev@gmail.com>
2025-04-26 11:31:48 +0300

    change schedule page

--- src/pages/schedule/ui/Schedule.vue
+++ src/pages/schedule/ui/Schedule.vue
@@ -30,12 +30,18 @@
         </div>
 
         selectedView: {{ selectedView }}
+        <br />
       </div>
     </div>
 
     <YearView v-if="selectedView.value === ViewUnits.Year" />
-    <MonthView v-else-if="selectedView.value === ViewUnits.Month" :events="MOCK_EVENTS" />
-    <DaysView v-else :events="MOCK_EVENTS" />
+    <MonthView
+      v-else-if="selectedView.value === ViewUnits.Month"
+      :events="MOCK_EVENTS"
+      @select-day="onSelectDay"
+      @select-week="onSelectWeek"
+    />
+    <DaysView v-else :events="MOCK_EVENTS" @select-day="onSelectDay" />
   </div>
 </template>
 
@@ -45,20 +51,22 @@ import { DaysView } from '@/widgets/schedule/views/days'
 import { MonthView } from '@/widgets/schedule/views/month'
 import { YearView } from '@/widgets/schedule/views/year'
 import { ViewSwitch, useViewSwitch, ViewUnits } from '@/features/schedule/view-switch'
-import type { IEvent } from '@/entities/schedule/event'
+import type { IEvent } from '@/entities/schedule/event-day'
 import { MONTHS } from '@/shared/constants/date'
+import { showDay, showWeek } from '../lib/date'
 
 const { selectedView } = useViewSwitch()
 
 const route = useRoute()
+const router = useRouter()
 const params = route.params
 
 const MOCK_EVENTS: IEvent[] = [
   {
     id: 1,
     name: 'Weekly team meeting',
-    startDate: new Date('2025.02.20'),
-    endDate: new Date('2025.02.20'),
+    startDate: new Date('2025.04.20'),
+    endDate: new Date('2025.04.20'),
     startTime: '03:00',
     endTime: '04:30',
     background: '#fa934b',
@@ -66,17 +74,17 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 111,
     name: 'Weekly team me!!!!!',
-    startDate: new Date('2025.02.20'),
-    endDate: new Date('2025.02.20'),
+    startDate: new Date('2025.04.20'),
+    endDate: new Date('2025.04.20'),
     startTime: '03:30',
     endTime: '05:25',
     background: '#fac34b',
   },
   {
     id: 2,
-    name: 'Weekly team meeting',
-    startDate: new Date('2025.02.20'),
-    endDate: new Date('2025.02.20'),
+    name: 'Some event',
+    startDate: new Date('2025.04.20'),
+    endDate: new Date('2025.04.20'),
     startTime: '09:25',
     endTime: '12:10',
     background: '#3bbd00',
@@ -84,8 +92,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 3,
     name: 'Weekly team meeting 2',
-    startDate: new Date('2025.02.21'),
-    endDate: new Date('2025.02.21'),
+    startDate: new Date('2025.04.21'),
+    endDate: new Date('2025.04.21'),
     startTime: '08:15',
     endTime: '14:25',
     background: '#d11c91',
@@ -93,8 +101,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 4,
     name: 'Weekly team meeting',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '03:00',
     endTime: '04:30',
     background: '#fa934b',
@@ -102,8 +110,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 5,
     name: 'Weekly team meeting',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '09:25',
     endTime: '12:10',
     background: '#3bbd00',
@@ -111,8 +119,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 6,
     name: 'Weekly team meeting 2',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '07:15',
     endTime: '11:30',
     background: '#d11c91',
@@ -120,8 +128,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 6,
     name: 'Weekly team meeting 2',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '14:15',
     endTime: '17:30',
     background: '#d1ad1c',
@@ -129,8 +137,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 7,
     name: 'Weekly team meeting 2',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '12:15',
     endTime: '19:45',
     background: '#d1521c',
@@ -138,8 +146,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 8,
     name: 'Weekly team meeting 23',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '18:10',
     endTime: '19:25',
     background: '#1cd1a1',
@@ -147,8 +155,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 9,
     name: 'Weekly team meeting 7',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '16:00',
     endTime: '17:00',
     background: '#ff4949',
@@ -156,17 +164,17 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 10,
     name: 'Weekly team meeting',
-    startDate: new Date('2025.02.22'),
-    endDate: new Date('2025.02.22'),
+    startDate: new Date('2025.04.22'),
+    endDate: new Date('2025.04.22'),
     startTime: '17:15',
     endTime: '18:10',
     background: '#3bbd30',
   },
   {
     id: 11,
-    name: 'Weekly team meeting',
-    startDate: new Date('2025.02.21'),
-    endDate: new Date('2025.02.23'),
+    name: 'Long',
+    startDate: new Date('2025.04.21'),
+    endDate: new Date('2025.04.23'),
     startTime: '22:15',
     endTime: '03:40',
     background: '#2b3e3c',
@@ -174,8 +182,8 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 13,
     name: 'Weekly team meeting 7',
-    startDate: new Date('2025.02.23'),
-    endDate: new Date('2025.02.23'),
+    startDate: new Date('2025.04.23'),
+    endDate: new Date('2025.04.23'),
     startTime: '00:00',
     endTime: '03:40',
     background: '#641cd1',
@@ -183,11 +191,19 @@ const MOCK_EVENTS: IEvent[] = [
   {
     id: 12,
     name: 'Weekly team meeting 7',
-    startDate: new Date('2025.02.23'),
-    endDate: new Date('2025.02.23'),
+    startDate: new Date('2025.04.23'),
+    endDate: new Date('2025.04.23'),
     startTime: '08:15',
     endTime: '12:45',
     background: '#d1521c',
   },
 ]
+
+const onSelectDay = (date: Date) => {
+  showDay(date, router)
+}
+
+const onSelectWeek = (date: Date) => {
+  showWeek(date, router)
+}
 </script>
