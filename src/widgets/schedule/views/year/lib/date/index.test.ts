c1a65dfa300f586aabea0a2f758260e26b1fbb05
Vitaly Garshin <garshin.dev@gmail.com>
2025-04-27 17:47:06 +0300

    add new test

---
+++ src/widgets/schedule/views/year/lib/date/index.test.ts
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest'
+import { getYearDates } from '@/widgets/schedule/views/year/lib/date/index.ts'
+
+describe('getYearDates', () => {
+  it('should return array of size 12', () => {
+    const year = 2025
+    const result = getYearDates(year)
+
+    expect(result.length).toBe(11)
+  })
+})
